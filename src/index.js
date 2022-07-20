47999a4056205c4e2a387f5c47425ba346321649
Ammar Daham <ammar.daham@hva.nl>
2022-07-20 14:55:46 +0300

    add slash to url

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+<meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
