6d996f9b35eba26ba4382fc714fb16d06d56770d
Ammar Daham <ammar.daham@hva.nl>
2022-07-20 16:11:22 +0300

    fixing url problem

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ import reportWebVitals from './reportWebVitals';
 
 <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />
 
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
